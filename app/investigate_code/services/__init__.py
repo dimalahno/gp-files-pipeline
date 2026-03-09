1359f013dcf5021ba43f8f554c0972a9adc964c3
dimalahno <dimalahno@rambler.ru>
2026-03-09 09:08:59 +0500

    Remove deprecated `parser_service.py` and update `__init__.py` to clean up codebase.

--- app/investigate_code/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .case_summary_service import generate_case_summary
-from .index_document_service import generate_index_document
-
-__all__ = ["generate_case_summary", "generate_index_document"]
